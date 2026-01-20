6783d9360c0c151105b7f10cfb06b9980bd01210
yupok77 <yupokl@nycstudents.net>
2026-01-20 19:01:34 +0000

    rrrrrrrrrrrrrrrr

--- WDLesson3_5Practical/script.js
+++ WDLesson3_5Practical/script.js
@@ -4,10 +4,22 @@
       3) Display the results in the appropriate element
 */
 function recArea(){
+      let len= parseFloat(document.getElementById("l").value);
+      let wid= parseFloat(document.getElementById("w").value);
+      let op= document.getElementById("output");
+      let A= len * wid;
+      op.innerHTML= A;
+
 
 }
 
 function recPerimeter(){
+      let len= parseFloat(document.getElementById("l").value);
+      let wid= parseFloat(document.getElementById("w").value);
+      let op= document.getElementById("output");
+      let P = 2*(len + wid);
+      op.innerHTML= P;
+
 
 }
 
@@ -19,13 +31,24 @@ function recPerimeter(){
 */
 let pi = 3.1415926;
 
-function cirArea(){
 
+
+
+function cirArea(){
+      let r = parseFloat( document.getElementById("rad").value);
+      let op = document.getElementById("output");
+      let A = pi * (r ** 2);
+      op.innerHTML = "Area of circle is: " A;
 }
+
 
 function cirPerimeter(){
-
+      let r = parseFloat( document.getElementById("rad").value);
+      let op = document.getElementById("output");
+      let c = 2*pi*r;
+      op.innerHTML = "Circumference of circle is: "C;
 }
+
 
 /* Challenge Bonus: Complete the area and perimeter functions below for the triangle.  Guidelines,
       1) Create variables and retrieve the information from the text inputs you.
