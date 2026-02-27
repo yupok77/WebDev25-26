188855f2db87759068b43b837431972d02da3dec
yupok77 <yupokl@nycstudents.net>
2026-02-27 19:01:50 +0000

    tt4t4t4

--- WDLesson4_4Practical/script.js
+++ WDLesson4_4Practical/script.js
@@ -1,15 +1,22 @@
 /* Challenge 2: Complete the function generateProblem by ,
        1) Create a variable for the <span> with id of problem
        2) Generate two random numbers between 0 and 20
-       3) Store the correct answer in the variable correct.  This will be used in checkProblem( )
+       3) Store the correct answer in the variable correct.  This will be used in checkResponse( )
        4) Using string interpolation, display an addition problem like "2 + 2 =" except using the random numbers generated. 
 */
 let correct;
 function generateProblem(){
+       let prob = document.getElementById("problem");
+       let n1 = Math.floor(Math.random()*(20-0+1)+0);
+       let n2 = Math.floor(Math.random()*21+0);
+       correct = n1 + n2;
+       prob. innerHTML = `${n1} + ${n2} = `;
+
+
 
 }
 
-/* Challenge 3: Complete the function checkProblem by ,
+/* Challenge 3: Complete the function checkResponse by ,
        1) Create a variable for the users response
        2) Create a variable for the <div> with id of output
        3) Create a variable msg for the output
@@ -18,8 +25,27 @@ function generateProblem(){
            b) otherwise set msg to "Incorrect. The answer is " followed by the correct answer.  Also include incorrect image
        4) Display msg in the output
 */
-function checkProblem(){
+function checkResponse(){
+       let user = parseInt(document.getElementById("response").value);
+       let op = document.getElementById("output");
+       let filename = "";
+       let msg = "";
 
+       if (user == correct){
+              filename = "correct.webp";
+              msg = `Correct! <br> <img src="${filename}">`;
+       }else{
+              filename = "incorrect.avif";
+              msg = `Incorrect. The correct answer is ${correct}. <br> <img src="${filename}">`;
+
+
+
+       }
+              
+       
+
+       op. innerHTML = msg;
 }
+
 
 
